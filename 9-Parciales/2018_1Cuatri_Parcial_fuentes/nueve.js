87b02fb549baef042cc45db46cfc078847911f3e
alumno <alumno@utnfra.com.ar>
2020-02-27 22:04:43 -0300

    Ejercicio 9 casi completo UNDEF

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -2,17 +2,22 @@ function mostrar()
 {
     var respuesta;
     var marca;
-    var producto;
+    var productos;
     var peso;
+    var pesoMaximo;
+    var pesoMinimo;
     var tempAlmacenamiento; 
-    var temperaturasPares;
+    var temperaturasPares = 0;
     var marcaProducPesado;
-    var productosCeroGrados; 
+    var productosCeroGrados = 0;
     var promedioPeso;    
     var max;
     var min;
+    var acumuladorPesos = 0;
+    var cantidadPeso = 0;
     
-    
+    //Creo un flag 
+    var primeraVez = true
     
   
     do{
@@ -25,6 +30,7 @@ function mostrar()
         do {
            marca = prompt ("Ingrese una marca")
              
+       
         } while (!isNaN(marca));
     
             do{
@@ -32,13 +38,60 @@ function mostrar()
             
             } while(isNaN(tempAlmacenamiento) || tempAlmacenamiento < -30 || tempAlmacenamiento > 30);
 
-            if(temperaturasPares / 2 == 0 ){
+            if(temperaturasPares % 2 == 0){
 
-                
+                temperaturasPares ++;
+            } 
+            
+            if (temperaturasPares < 0){
+
+                productosCeroGrados ++;
             }
+
+
+            // evaluamos primera vez para calcular peso maximo y minimo
+            
+            if (primeraVez){
+
+                primeraVez = false;
+                pesoMaximo = peso;
+                pesoMinimo = peso;
+                marcaProducPesado = marca;
+            } else {
+
+                if (peso>pesoMaximo){
+
+                    pesoMaximo = peso;
+                    marcaProducPesado = marca;
+                } else if (peso<pesoMinimo){
+
+                    pesoMinimo = peso;
+
+                }
+            }
+
+
+
     
         respuesta = confirm("Desea continuar?");
     
     } while(respuesta);  
+
+    if(productos > 0){
+
+        promedioPeso = acumuladorPesos / productos
+    } else {
+
+        promedioPeso = "No ingreso ningun producto";
+    }
+
+    document.write("La cantidad de temperaturas pares: " + temperaturasPares + "<br>");
+    document.write("La marca del producto mas pesado: " + marcaProducPesado + "<br>");
+    document.write("La cantidad de productos que se conservan a menos grados: " + productosCeroGrados + "<br>");
+    document.write("El promedio de todos los pesos: " + promedioPeso + "<br>");
+    document.write("El peso maximo: " + max + "<br>");
+    document.write("El peso minimo: " + min + "<br>");
+
+
 
 }
