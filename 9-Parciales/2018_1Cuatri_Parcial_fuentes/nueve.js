5bbc44ad289964b7c79deb889154afa927c0b1b6
alumno <alumno@utnfra.com.ar>
2020-02-27 21:19:40 -0300

    Ultima clase Ejs 7, 8 y 9

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,44 @@
 function mostrar()
 {
+    var respuesta;
+    var marca;
+    var producto;
+    var peso;
+    var tempAlmacenamiento; 
+    var temperaturasPares;
+    var marcaProducPesado;
+    var productosCeroGrados; 
+    var promedioPeso;    
+    var max;
+    var min;
+    
+    
+    
+  
+    do{
+       
+       do{
+           peso = prompt("Ingrese un peso entre 1 y 100");
+           peso = parseInt(peso);
+       } while(isNaN(peso) || peso < 1 || peso > 100);
+    
+        do {
+           marca = prompt ("Ingrese una marca")
+             
+        } while (!isNaN(marca));
+    
+            do{
+                tempAlmacenamiento = prompt("Ingrese una temperatura entre -30 y 30");
+            
+            } while(isNaN(tempAlmacenamiento) || tempAlmacenamiento < -30 || tempAlmacenamiento > 30);
+
+            if(temperaturasPares / 2 == 0 ){
+
+                
+            }
+    
+        respuesta = confirm("Desea continuar?");
+    
+    } while(respuesta);  
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
 
+var respuesta;
+var  
+
+
+
+//se asigna bandera solo para la primera vez de la iteracion 
+
+
+
+
+
 }
