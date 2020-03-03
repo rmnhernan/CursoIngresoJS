2d6547fd02413d1493187e8cea1c828afea36f98
ROMAN FEDERICO <ROMANF@sevillanita.com>
2020-03-03 14:02:39 -0300

    Ejercicio 5 terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -3,20 +3,25 @@ var cantidadPersonas;
 var cantidadDiasEstadia;
 var formaPago;
 var respuesta;
-var maxReservas;
+var maxReservas = 0;
 var primeraVez = true
 var maxNombre;
-var maxDiasEstadia;
+var maxDiasEstadia = 0;
 var contadorEfectivo = 0;
 var contadorTarjeta = 0;
 var contadorQR = 0;
+var contadorReservas = 0;
+var acumuladorDias = 0;
+var contadorReservas = 0;
+var promedioDias;
+var maxFormaPago;
 
 
 function mostrar()
 {
 
-respuesta = confirm("Desea ingresar sus datos?");
-
+    respuesta = confirm("Desea ingresar sus datos?");
+    
 do{
 
     do{
@@ -25,58 +30,81 @@ do{
             break;
         }
     } while (!isNaN(nombreHuesped));
-
+    
     do{
         cantidadPersonas = prompt("Ingrese la cantidad de personas");
         cantidadPersonas = parseInt(cantidadPersonas);
         if (cantidadPersonas > 15){
             cantidadPersonas = alert("No se permiten mas de 15 huespedes");
-            }
+        }
     } while (isNaN(cantidadPersonas) || cantidadPersonas < 1 || cantidadPersonas > 16);
-
+    
     do{
         cantidadDiasEstadia = prompt("Ingrese sus dias de estadía");
         cantidadDiasEstadia = parseInt(cantidadDiasEstadia);
-
+        
         if(cantidadDiasEstadia > 35){
             cantidadDiasEstadia = alert("La reserva máxima es de 35 días");
         }
 
     } while (isNaN(cantidadDiasEstadia) || cantidadDiasEstadia < 0 || cantidadDiasEstadia > 36);
-
+    
     do{
         formaPago = prompt("ingrese su método de pago (Efectivo/Tarjeta/QR)").toLowerCase();
-                    
-
+        
+        
     } while(!isNaN(formaPago) || formaPago != "efectivo" && formaPago != "tarjeta" && formaPago != "qr");
 
-//Definir maximos (Dias de estadía y reservas):
-
-    if(primeraVez){
+    //Datos del promedio de dias de reserva:
+    //COMMENT = VA EN ESTE ORDEN, SI NO NO PROMEDIA***
+    acumuladorDias += cantidadDiasEstadia;
+    contadorReservas ++;
+    
+    //No es necesario el FLAG; lo reemplazo por un máximo igual a 0 (O al número mas chico posible)
+    
+    /*if(primeraVez){
         primeraVez = false;
         maxReservas = cantidadPersonas;
         maxNombre = nombreHuesped;
         maxDiasEstadia = cantidadDiasEstadia;
-                
-    } else if(cantidadPersonas > maxReservas){
-
-            maxReservas = cantidadPersonas;
-            maxNombre = nombreHuesped;
-
-        } else if(cantidadDiasEstadia > maxDiasEstadia){
-
-                maxDiasEstadia = cantidadDiasEstadia;
-        }        
+    }
+        */
+        
+     if(cantidadPersonas > maxReservas){
+        
+        maxReservas = cantidadPersonas;
+        maxNombre = nombreHuesped;
+        
+    } 
+    
+     if(cantidadDiasEstadia > maxDiasEstadia){
+        
+        maxDiasEstadia = cantidadDiasEstadia;
+        maxReservas = cantidadPersonas;
+        
+    }        
 
     //Forma de pago más utilizada 
     if (formaPago == "efectivo"){
         contadorEfectivo ++;
-    } else if(formaPago == "tarjeta"){
-        contadorTarjeta ++;
-    } else {
-        contadorQR ++
-    }
-        
+        } else if(formaPago == "tarjeta"){
+            contadorTarjeta ++;
+            } else if(formaPago = "qr"){
+                contadorQR ++
+            }
+
+    if (contadorEfectivo > contadorTarjeta && contadorTarjeta > contadorQR){
+        maxFormaPago = "efectivo"
+        } else if (contadorTarjeta > contadorEfectivo && contadorTarjeta > contadorQR){
+            maxFormaPago = "tarjeta";
+            } else if(contadorQR > contadorEfectivo && contadorQR > contadorTarjeta){
+                maxFormaPago = "qr";
+                }   
+    
+    //Calculo promedio
+    
+    promedioDias = acumuladorDias / contadorReservas;
+    promedioDias = parseInt(promedioDias);
 
    
     respuesta = confirm("Desea continuar ingresando datos?");
@@ -87,4 +115,7 @@ do{
 
 alert("El huespued que trajo más personas en una sola reserva es " + maxNombre);
 alert("La cantidad de personas que se quedaron más días es de " + maxReservas + " y se quedaron " + maxDiasEstadia + " días");
+alert("La forma de pago más utilizada fue " + maxFormaPago);
+alert("El promedio de cantidad de días por reserva fue de " + promedioDias);
 }
+
