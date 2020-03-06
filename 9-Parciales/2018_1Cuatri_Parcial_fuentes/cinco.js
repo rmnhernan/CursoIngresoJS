db8929b7b80ccb3130e97234075e346d0f856236
ROMAN FEDERICO <ROMANF@sevillanita.com>
2020-03-06 16:38:32 -0300

    Ej 5 PROMEDIO MAL

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,121 +1,125 @@
-var nombreHuesped;
-var cantidadPersonas;
-var cantidadDiasEstadia;
-var formaPago;
 var respuesta;
-var maxReservas = 0;
-var primeraVez = true
-var maxNombre;
-var maxDiasEstadia = 0;
-var contadorEfectivo = 0;
-var contadorTarjeta = 0;
+var nombreHuesped;
+var personas;
+var diasEstadia;
+var formaPago;
+var formaPagoMasUsada = 0;
+var contadorEfectivo = 0
+var contadorTarjeta = 0
 var contadorQR = 0;
-var contadorReservas = 0;
+var huespedMaspersonas;
+var maxPersonas = 0;
+var personasMasDias;
+var maxDias = 0;
 var acumuladorDias = 0;
 var contadorReservas = 0;
-var promedioDias;
-var maxFormaPago;
+var promedoDias;
+
+
 
 
 function mostrar()
-{
-
-    respuesta = confirm("Desea ingresar sus datos?");
-    
-do{
-
+{ 
     do{
-        nombreHuesped = prompt("Ingrese su nombre");
-        if(nombreHuesped == null){
-            break;
-        }
-    } while (!isNaN(nombreHuesped));
-    
-    do{
-        cantidadPersonas = prompt("Ingrese la cantidad de personas");
-        cantidadPersonas = parseInt(cantidadPersonas);
-        if (cantidadPersonas > 15){
-            cantidadPersonas = alert("No se permiten mas de 15 huespedes");
-        }
-    } while (isNaN(cantidadPersonas) || cantidadPersonas < 1 || cantidadPersonas > 16);
-    
-    do{
-        cantidadDiasEstadia = prompt("Ingrese sus dias de estadía");
-        cantidadDiasEstadia = parseInt(cantidadDiasEstadia);
-        
-        if(cantidadDiasEstadia > 35){
-            cantidadDiasEstadia = alert("La reserva máxima es de 35 días");
-        }
-
-    } while (isNaN(cantidadDiasEstadia) || cantidadDiasEstadia < 0 || cantidadDiasEstadia > 36);
-    
-    do{
-        formaPago = prompt("ingrese su método de pago (Efectivo/Tarjeta/QR)").toLowerCase();
-        
-        
-    } while(!isNaN(formaPago) || formaPago != "efectivo" && formaPago != "tarjeta" && formaPago != "qr");
-
-    //Datos del promedio de dias de reserva:
-    //COMMENT = VA EN ESTE ORDEN, SI NO NO PROMEDIA***
-    acumuladorDias += cantidadDiasEstadia;
-    contadorReservas ++;
-    
-    //No es necesario el FLAG; lo reemplazo por un máximo igual a 0 (O al número mas chico posible)
-    
-    /*if(primeraVez){
-        primeraVez = false;
-        maxReservas = cantidadPersonas;
-        maxNombre = nombreHuesped;
-        maxDiasEstadia = cantidadDiasEstadia;
-    }
-        */
-        
-     if(cantidadPersonas > maxReservas){
-        
-        maxReservas = cantidadPersonas;
-        maxNombre = nombreHuesped;
-        
-    } 
-    
-     if(cantidadDiasEstadia > maxDiasEstadia){
-        
-        maxDiasEstadia = cantidadDiasEstadia;
-        maxReservas = cantidadPersonas;
-        
-    }        
-
-    //Forma de pago más utilizada 
-    if (formaPago == "efectivo"){
-        contadorEfectivo ++;
-        } else if(formaPago == "tarjeta"){
-            contadorTarjeta ++;
-            } else if(formaPago = "qr"){
-                contadorQR ++
+        do{
+            nombreHuesped = prompt("Ingrese su nombre");
+            if (nombreHuesped == null){
+                break;
             }
-
-    if (contadorEfectivo > contadorTarjeta && contadorTarjeta > contadorQR){
-        maxFormaPago = "efectivo"
-        } else if (contadorTarjeta > contadorEfectivo && contadorTarjeta > contadorQR){
-            maxFormaPago = "tarjeta";
-            } else if(contadorQR > contadorEfectivo && contadorQR > contadorTarjeta){
-                maxFormaPago = "qr";
-                }   
-    
-    //Calculo promedio
-    
-    promedioDias = acumuladorDias / contadorReservas;
-    promedioDias = parseInt(promedioDias);
-
-   
-    respuesta = confirm("Desea continuar ingresando datos?");
-
+        } while (!isNaN(nombreHuesped))
         
+        do{
+            personas = prompt("Ingrese la cantidad de personas");
+            personas = parseInt(personas);
+        } while(isNaN(personas) || personas < 1);
+        
+        do{
+            diasEstadia = prompt("Ingrese la cantidad de días de estadía");
+            diasEstadia = parseInt(diasEstadia);
+            
+        } while (isNaN(diasEstadia) || diasEstadia < 1);
+        
+        do{
+            formaPago = prompt("Ingrese la forma de pago");
+        } while (formaPago != "efectivo" && formaPago != "tarjeta" && formaPago != "qr");
+        
+        contadorReservas ++;
+        acumuladorDias += diasEstadia;
+        promedioDias = acumuladorDias / contadorReservas;
+        promedioDias = parseInt(promedoDias);
+        
+        if (personas > maxPersonas){
+            
+            maxPersonas = personas;
+            huespedMaspersonas = nombreHuesped;
+        }
+        
+         
+         if(diasEstadia > maxDias){
+             maxDias = diasEstadia;
+             personasMasDias = personas;
+         }
 
-} while (respuesta);
 
-alert("El huespued que trajo más personas en una sola reserva es " + maxNombre);
-alert("La cantidad de personas que se quedaron más días es de " + maxReservas + " y se quedaron " + maxDiasEstadia + " días");
-alert("La forma de pago más utilizada fue " + maxFormaPago);
-alert("El promedio de cantidad de días por reserva fue de " + promedioDias);
+         switch (formaPago){
+             case "efectivo":
+                 contadorEfectivo ++;
+                 if(contadorEfectivo > formaPagoMasUsada){
+                    formaPagoMasUsada = contadorEfectivo;
+                    stringPago = formaPago;
+                 }
+                 break;
+             case "tarjeta":
+                 contadorTarjeta ++;
+                 if(contadorTarjeta > formaPagoMasUsada){
+                    formaPagoMasUsada = contadorTarjeta;
+                    stringPago = formaPago;
+                }
+                 break;
+             case "qr":
+                 contadorQR ++;
+                 if(contadorEfectivo > formaPagoMasUsada){
+                    formaPagoMasUsada = contadorQR;
+                    stringPago = formaPago;
+                }
+                 break;    
+        }
+
+       
+
+         /*
+         if (formaPago == "efectivo"){
+             contadorEfectivo ++;
+             if(contadorEfectivo > formaPagoMasUsada){
+                 formaPagoMasUsada = contadorEfectivo;
+                 stringPago = formaPago;
+                } 
+            }
+            
+            
+            if (formaPago == "tarjeta" ){
+                contadorTarjeta ++;
+                if(contadorTarjeta > formaPagoMasUsada){
+                    formaPagoMasUsada = contadorTarjeta;
+                    stringPago = formaPago;
+                }
+            }
+            
+            if (formaPago == "qr"){
+                contadorEfectivo ++;
+                if(contadorEfectivo > formaPagoMasUsada){
+                    formaPagoMasUsada = contadorQR;
+                    stringPago = formaPago;
+                }
+            }*/
+            
+            respuesta = confirm("Desea seguir ingresando datos?");
+            
+        } while (respuesta);
+
+alert("La forma de pago más utilizada fue " + stringPago);
+alert ("El huesped que trajo más personas fue "+ huespedMaspersonas);
+alert ("La cantidad de personas que se quedaron más días es de "+ personasMasDias);
+alert ("El promedio de cantidad de días por reserva fue de " + promedioDias);
 }
 
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,69 +1,27 @@
 
- var nota;
- var sexo;
- var promedio;
- var contador = 0;
- var acumuladorNotas = 0;
- var notaMasBaja;
- var sexoNotaBaja;
- var contadorVarones = 0;
+var contador = 0;
+var sexo;
+var nota;
+
 
 function mostrar()
-{
-    while (contador < 5){
-        contador ++;
-console.log(contador)
+{ while(contador < 5){
+    contador ++;
+    
+    
+    do{
 
-//iteraciones hasta que el usuario quiera:
-//while(respuesta)
-// respuesta = confirm() / respuesta "si" / ingrese la cantidad
-
-        do {
-            nota = prompt ("Ingrese la nota");
-            nota = parseInt(nota);
+        nota = prompt("Ingrese la nota");
+        nota = parseInt(nota);
         
-        } while (isNaN(nota) || nota < 0 || nota > 10);
+    } while (isNaN(nota) || nota < 0 || nota > 10);
+    
+}   do{
+    
+    sexo = prompt("Ingrese el sexo del alumno");
+    
+} while (sexo != "f" && sexo != "m");
 
-        do {
-                sexo = prompt("Ingrese un sexo (f o m)").toLowerCase();
-            
-            } while (sexo != "f" && sexo != "m");
 
-            
-            
-            
-            
-            //tener cuidado de no dividir por cero
-            // en este caso sabemos que contador NUNCA es igual a cero
-            
-            // Punto B
-            
-            if (contador == 1){
-                
-                notaMasBaja = nota;
-                sexoNotaBaja = sexo
-                
-            } else if (contador > 1 && nota < notaMasBaja){
-                notaMasBaja = nota;
-                sexoNotaBaja = sexo
-            }
-            
-            
-            // Punto C
-            
-            if (nota >= 6 && sexo == "m"){
-                
-                contadorVarones ++;
-            }
-            // Punto A   ***LOS ACUMULADORES TIENEN VALOR 0 EN  LA VARIABLE***
-            
-            acumuladorNotas += nota;
-            
-            promedio = acumuladorNotas / contador;    
-        }
-            
-            console.log("El promedio de las notas totales es " + promedio);
-            console.log("La nota más baja es " + notaMasBaja + " y el sexo de esa persona es " + sexoNotaBaja);
-            console.log("La cantidad de varones que su nota haya sido mayor o igual a 6 es " + contadorVarones);
-            
-}
+
+} 
